d9ce72b4d4ebe78a2a42a0375405350037f516e4
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-05-14 14:28:01 -0300

    Update script.js

--- variáveis #1/script.js
+++ variáveis #1/script.js
@@ -35,3 +35,15 @@ let n1 = 15;
 let n2 = 20;
 let soma = n1 + n2 
 console.log(soma);
+
+/* String e Template String */
+
+let nome2 = "Gleice";
+let sobrenome2 = "Kelly";
+let nomeCompleto2 = `${nome2} ${sobrenome2}`;
+
+console.log("Nome: " + nomeCompleto2);
+
+let idade1 = 32;
+let idadeString = `${idade1} anos`
+console.log(idadeString);
