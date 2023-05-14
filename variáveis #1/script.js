8e73b8d5a6d6d940aff20807c4a82e7d6e4a82ad
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-05-14 14:03:02 -0300

    Create script.js

---
+++ variáveis #1/script.js
@@ -0,0 +1,24 @@
+let nome = "gleice"
+let idade = 32
+let cidade1 = "Lisboa"
+let pais = "Portugal"
+<br>
+    
+console.log("Meu nome é " + nome + "," + " tenho " + idade + " anos, quero morar em " + cidade1 + "/" + pais)
+
+let nomeCompleto = { nome: "Gleice", idade: 32};
+<br>
+  
+console.log(typeof nomeCompleto)
+<br>
+
+let carro = "Ferrari"
+console.log(carro)
+<br>
+ 
+let bolo = 19.99
+console.log(bolo)
+<br>
+  
+let cidade = "São Paulo"
+console.log(cidade)
