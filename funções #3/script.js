9103e97ce215d30676641cdb1b2bda32f42c5a77
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-05-21 20:02:58 -0300

    Update script.js

--- funções #3/script.js
+++ funções #3/script.js
@@ -89,6 +89,29 @@ if(validacao) {
 } else {
     console.log("Acesso negado");
 }
+   
+//-----------------------------------------//
+ funciona calcularImovel(metragem, quartos) {
+    let m2 = 3000;
+    let preco = 0;
+     
+    switch(quartos) {
+       case1:
+           preco = metragem * m2;
+            break;
+       case2:
+           preco = metragem * (m2 * 1.2);
+           break;
+       case3:
+           preco = metragem * (m2 * 1.5);
+           break;
     
-    
+
+      }
+      return preco;
+ }
+ let metragem = 123;
+ let quartos = 3;
+ let preco = calcularImovel(metragem, quartos);
+ console.log(`A casa custa R$ ${preco});
     
