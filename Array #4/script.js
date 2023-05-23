896b3112bf335fc2364ebc0b0f9e33fb07c24620
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-05-23 18:31:05 -0300

    Update script.js

--- Array #4/script.js
+++ Array #4/script.js
@@ -1,4 +1,10 @@
-//1.No array abaixo, qual o número que pega a Ferrari?
+//1. No array abaixo, qual o número que pega a Ferrari?
 let carros = ['BMW', 'Ferrari', 'Mercedes'];
 let x = [1];
 console.log('1. ' + carros[x]);
+
+//--------------------------------------------------//
+//2. Troque a Ferrari por Audi
+console.log('2. Lista com Audi: ');
+console.log(carros);
+carros[1] = 'Audi';
