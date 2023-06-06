faabddc0e5b93b115529e9742d65a64a6806ff23
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-06-06 14:11:51 -0300

    Update script.js

--- loops #6/script.js
+++ loops #6/script.js
@@ -3,3 +3,11 @@ let fruits = ['Maça', 'Uva', 'Banana'];
 for(let i = 0; i < fruits.length; i++) {
     console.log(fruits[i]);
 }
+
+//---------------------------------------//
+//2.Conte de 1 até 100 através de um While:
+let number = 1;
+while(number <= 100) {
+  console.log(number);
+  number++;
+}
