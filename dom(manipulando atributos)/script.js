c9dfd680c77e8bade3775c812a359b89a8390f7c
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-06-11 14:17:59 -0300

    Create script.js

---
+++ dom(manipulando atributos)/script.js
@@ -0,0 +1,12 @@
+function clicar() {
+    const teste = document.querySelector('#teste1');
+    const ul = teste.querySelector('ul');
+    //ul.innerHTML = "<li>Item alterado</li>"; //Alterou o conteúdo do 'ul'.
+    ul.innerHTML = ul.innerHTML + "<li>Item Alterado</li>" //Altera e adiciona o conteúdo no 'ul'.
+    //ul.children[0].innerHTML ="Item<strong> Alterado</strong>!"
+
+    //ul.outerHTML = "<strong>Aterado!</strong>"; //pega o elemento externo e altera todo o elemento, é pouco utilizado.
+
+    console.log(ul.innerHTML); //Conteúdo que tem dentro do elemento, neste caso dentro do "ul"
+    
+} 
