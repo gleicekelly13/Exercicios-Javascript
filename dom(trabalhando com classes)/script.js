6f166d6589847a41180bb815277fc40d4aa25515
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-06-15 19:13:46 -0300

    Create script.js

---
+++ dom(trabalhando com classes)/script.js
@@ -0,0 +1,10 @@
+/* clicar () {
+    const button = document.querySelector('button');
+
+    /*if(button.classList.contains('azul')) {
+        button.classList.remove('azul');
+        button.classList.add('verde');
+    } else {
+        button.classList.add('azul');
+        button.classList.remove('verde');
+    } 
