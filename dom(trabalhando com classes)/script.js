dcf6cebbed5cedc5106f7f1d7221f0d863d817e7
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-06-16 18:59:39 -0300

    Update script.js

--- dom(trabalhando com classes)/script.js
+++ dom(trabalhando com classes)/script.js
@@ -20,3 +20,21 @@
         button.classList.replace('verde', 'azul');
     } 
 } */
+
+function verde () {
+    document.querySelector('#exemplo').classList.remove('vermelho');
+    document.querySelector('#exemplo').classList.remove('azul');
+    document.querySelector('#exemplo').classList.add('verde');
+}
+
+function vermelho () {
+    document.querySelector('#exemplo').classList.remove('verde');
+    document.querySelector('#exemplo').classList.remove('azul');
+    document.querySelector('#exemplo').classList.add('vermelho');
+}
+
+function azul () {
+    document.querySelector('#exemplo').classList.remove('vermelho');
+    document.querySelector('#exemplo').classList.remove('verde');
+    document.querySelector('#exemplo').classList.add('azul');
+}
