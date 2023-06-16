41bf4105c7e5ba019ead8286481121c9102d4968
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-06-16 18:58:34 -0300

    Update script.js

--- dom(trabalhando com classes)/script.js
+++ dom(trabalhando com classes)/script.js
@@ -8,3 +8,15 @@
         button.classList.add('azul');
         button.classList.remove('verde');
     } 
+
+//----------------------------------------------------------//
+//button.classList.toggle('azul'); se não tiver, ele adiciona; se tiver, ele remove
+    
+    //button.classList.replace('azul', 'verde');
+
+    if(button.classList.contains('azul')) {
+        button.classList.replace('azul', 'verde');
+    } else {
+        button.classList.replace('verde', 'azul');
+    } 
+} */
