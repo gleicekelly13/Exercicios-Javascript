eaf98aa926033fafa4137d063e6686aabf5caab1
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-06-17 13:51:48 -0300

    Update script.js

--- dom(eventos de teclado)/script.js
+++ dom(eventos de teclado)/script.js
@@ -5,3 +5,13 @@
 function segurar () {
     console.log("SEGUROU!");
 }  */
+
+/*function soltar (e) {
+    console.log('TECLA APERTADA: ' + e.code);
+    console.log('SHIFT' + e.shiftKey);
+}
+
+const input =document.querySelector('input');
+input.addEventListener('keyup', soltar); //Aplica o evento diretamente no input 
+
+/*document.addEventListener('keyup', soltar); //funciona em qualquer lugar do documento */
