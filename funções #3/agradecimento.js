04e86baad691b5e0960be4bef3b670970e634d32
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-07-28 11:14:12 -0300

    Create agradecimento.js

---
+++ funções #3/agradecimento.js
@@ -0,0 +1,5 @@
+function sayThanks(name) {  /*Os parâmetros permitem que as funções aceitem entradas e executem uma tarefa usando as entradas. Usamos parâmetros como marcadores de posição para informações que serão passadas para a função quando ela for chamada. */
+    console.log('Thank you for your purchase, ' + name + '! We appreciate your business.')
+  }
+  
+  sayThanks('Cole');  //Os valores que são passados ​​para a função quando ela é chamada são chamados de argumentos .
