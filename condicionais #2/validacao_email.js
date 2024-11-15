ce1921f95f53025cff89ba5ee25bcd17d9007c9f
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-11-15 16:22:53 -0300

    Update validacao_email.js

--- condicionais #2/validacao_email.js
+++ condicionais #2/validacao_email.js
@@ -2,7 +2,7 @@ let email_acesso = "gleice.kelly22@hotmail.com";
 
 let email_cadastro = "GLEICE.KELLY22@hotmail.com";
 
-if(email_acesso.toLocaleLowerCase() == email_cadastro.toLocaleLowerCase()) {
+if(email_acesso.toLowerCase() == email_cadastro.toLowerCase()) {
 
     console.log("Foi enviado um link de ativação para o seu email");
 } else {
