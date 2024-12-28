bfb8998a8e89c0e09932369267998d2940d90fe6
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-12-28 10:31:18 -0300

    Create aritmethicsOperators.js

---
+++ Operadores/aritmethicsOperators.js
@@ -0,0 +1,18 @@
+/*
+Escreva um programa que faça o seguinte:
+
+Receba dois números (a e b) e exiba:
+A soma, subtração, multiplicação, divisão e resto da divisão entre a e b.
+A potência de a elevado a b.
+*/
+
+let a = 2;
+let b = 3;
+let soma = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let modulo = a % b;
+let potencia = a ** b;
+
+console.log(`A soma entre 2 e 3 é: ${soma};\n A subtração de 2 e 3 é: ${subtracao};\n A multiplicação entre 2 e 3 é: ${multiplicacao};\n A divisão de 2 por 3 é: ${divisao};\n O resto da divisão de 2 por 3 é: ${modulo};\n O número 2 elevado à 3° potência é: ${potencia}`)
