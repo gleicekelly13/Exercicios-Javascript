e0d2b9062f00d16288d0b7d1e37415b2abda3ee5
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-12-29 16:17:27 -0300

    Create assignmentOperators.js

---
+++ Operadores/assignmentOperators.js
@@ -0,0 +1,18 @@
+/*
+Crie uma variável chamada valor e atribua a ela o número 20.
+Usando operadores de atribuição, faça:
+Adicione 10 ao valor.
+Subtraia 5 do valor.
+Multiplique o valor por 2.
+Divida o valor por 4.
+Encontre o resto da divisão por 3.
+*/
+
+let valor = 20;
+
+console.log("Adicionando 10:", valor += 10);
+console.log("Retirando 5:", valor -= 5);
+console.log("Multiplicando por 2:", valor *= 2);
+console.log("Dividindo por 4:",  valor /= 4);
+console.log("Encontrando o resto da divisão por 3:", valor %= 3);
+
