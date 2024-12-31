2b3ba794b7b2d7da5bc9660d98bd44f1a2840965
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-12-31 14:20:08 -0300

    Create comparisonOperators.js

---
+++ Operadores/comparisonOperators.js
@@ -0,0 +1,17 @@
+/*
+Operadores de Comparação
+Crie um programa que:
+
+Compare dois números (x e y) e exiba no console:
+Se x é maior que y.
+Se x é igual a y (use tanto == quanto === para observar a diferença).
+Se x é diferente de y.
+*/
+
+let x = 10;
+let y = "10";
+
+console.log("x é maior que y? ", x < y);
+console.log("x é maior ou igual a y?", x <= y);
+console.log("x é igual a y? ", x == y, x === y);
+console.log("x é diferente de y? ", x != y, x !== y);
