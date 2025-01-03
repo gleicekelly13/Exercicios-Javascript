fc2fcda35e0cf210ba9841f6dc82d6501ffcd262
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2025-01-03 18:59:31 -0300

    Create maiorNumero.js

---
+++ condicionais #2/maiorNumero.js
@@ -0,0 +1,12 @@
+/*
+Solicite ao usuário dois números e imprima o maior deles usando a estrutura if/else.
+*/
+
+let a = 10;
+let b = 5;
+
+if(a > b) {
+    console.log(`O número ${a} é maior que o número ${b}`);
+} else {
+    console.log(`O número ${b} é maior que o número ${a}`);
+}
