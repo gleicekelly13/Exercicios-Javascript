6cb9b14535728996fd86b28d7b3182bc7dbe87ac
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2025-01-03 19:00:10 -0300

    Create imparPar.js

---
+++ condicionais #2/imparPar.js
@@ -0,0 +1,8 @@
+/*
+Peça um número ao usuário e verifique se ele é par ou ímpar usando o operador ternário.
+*/
+
+let numero = 10;
+numero % 2 === 0 ? console.log("O número " + numero + " é par!") : console.log("O número " + numero + " é ímpar!");
+
+
