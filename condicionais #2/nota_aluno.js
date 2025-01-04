1eefad32627ab01f2da13d944f986317ea573e15
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2025-01-04 17:06:49 -0300

    Create nota_aluno.js

---
+++ condicionais #2/nota_aluno.js
@@ -0,0 +1,11 @@
+/*
+Solicite uma nota de 0 a 10 e use if/else para verificar se o aluno foi aprovado (nota >= 7) ou reprovado.
+*/
+
+let nota = 10;
+
+if (nota >= 7) {
+    console.log(`Nota ${nota} - Aprovado`);
+} else {
+    console.log(`Nota ${nota} - Reprovado`);
+}
