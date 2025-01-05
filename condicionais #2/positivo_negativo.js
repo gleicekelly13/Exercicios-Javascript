4b123e3668c7462ebfbdc64d735fe681dea6c6b4
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2025-01-05 14:39:43 -0300

    Create positivo_negativo.js

---
+++ condicionais #2/positivo_negativo.js
@@ -0,0 +1,13 @@
+/*
+Solicite um número ao usuário e use if/else para verificar se o número é positivo, negativo ou zero.
+*/
+
+let numero = 10;
+
+if(numero > 0) {
+    console.log(`O número ${numero} é positivo!`);
+} else if(numero < 0) {
+    console.log(`O número ${numero} é negativo!`);
+} else {
+    console.log(`O número ${numero} é neutro!`);
+}
