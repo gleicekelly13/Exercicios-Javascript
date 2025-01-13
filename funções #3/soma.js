524d49397ddf7d608ddbf6adef1dea02af972cbd
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2025-01-13 15:14:11 -0300

    Create soma.js

---
+++ funções #3/soma.js
@@ -0,0 +1,13 @@
+/*
+Soma de dois números
+Crie uma função chamada somar que receba dois números como argumentos e retorne a soma deles.
+*/
+
+function somar (a,b) {
+    return a + b;
+}
+
+let a = 2;
+let b = 10;
+
+console.log(somar(2,10));
