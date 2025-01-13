14af52609024967908047ca454a58d14f1f8ff81
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2025-01-13 15:15:22 -0300

    Create saudacao.js

---
+++ funções #3/saudacao.js
@@ -0,0 +1,12 @@
+/*
+Saudação personalizada
+Crie uma função chamada saudacao que receba um nome como argumento 
+e retorne a mensagem: "Olá, [nome]! Seja bem-vindo(a)!"
+*/
+
+function saudacao (nome) {
+    return nome;
+}
+
+let nome = "Gleice";
+console.log(`Olá, ${nome}! Seja bem-vinda!"`);
