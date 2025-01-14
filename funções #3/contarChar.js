a687b4aeea035b7f07a1de0c17478d9d3c1f701c
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2025-01-14 10:45:13 -0300

    Create contarChar.js

---
+++ funções #3/contarChar.js
@@ -0,0 +1,14 @@
+/*
+Crie uma função chamada contarCaracteres que receba uma string como argumento 
+e retorne o número de caracteres nela.
+*/
+
+function contarCaracteres (str) {
+    return str.length;
+}
+
+let palavra = "Olá Mundo!";
+console.log(contarCaracteres(palavra.trim()));
+
+
+//str === string
