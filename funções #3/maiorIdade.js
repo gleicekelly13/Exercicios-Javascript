83e737b09fc0881ae12b3ff3cfdbe420fb759c71
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2025-01-16 10:24:55 -0300

    Create maiorIdade.js

---
+++ funções #3/maiorIdade.js
@@ -0,0 +1,18 @@
+/*
+Verificar se é maior de idade
+Crie uma função chamada ehMaiorDeIdade que receba a idade de uma pessoa como argumento 
+e retorne true se ela for maior de idade (18 anos ou mais), e false caso contrário.
+*/
+
+function ehMaiorDeIdade (idade) {
+    if (idade >= 18) {
+        return true
+    } else {
+        return false;
+    }
+}
+
+let idade = 34;
+let validaIdade = ehMaiorDeIdade(idade);
+
+console.log(`Idade equivalente a ${idade} é maior de idade?: ${validaIdade}`);
