9f2799fd7b0ffe9b4d9870d35fe5a8a6d0d7b3cb
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2025-01-20 10:51:38 -0300

    Create somaNumeros.js

---
+++ loops #6/somaNumeros.js
@@ -0,0 +1,10 @@
+/*
+Use um loop for para calcular a soma dos números de 1 a 100.
+*/
+
+let total = 0;
+
+for(let number = 1; number <= 100; number++) {
+    total = total + number;  //A variável `total` acumula o resultado das somas
+    console.log(total);
+}
