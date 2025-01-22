2f1374b0ac623dacddb354ed7ef450f6eeb0becd
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2025-01-22 11:01:54 -0300

    Create index.js

---
+++ loops #6/Tabuada_plus/index.js
@@ -0,0 +1,25 @@
+/*
+Modifique a tabuada para aceitar um intervalo de números (ex: tabuada de 1 a 5).
+Solicitar ao usuário dois números: um número inicial e um número final.
+Mostrar a tabuada para todos os números nesse intervalo.
+Use um loop para percorrer todos os números entre inicio e fim.
+Dentro do loop, exiba a tabuada de cada número (de 1 a 10).
+*/
+
+import entradaDados from 'readline-sync';
+
+let inicio = parseInt(entradaDados.question('Digite um numero inicial: '));
+let fim = parseInt(entradaDados.question('Digite um numero final: '));
+
+console.log(`\nTabuada de ${inicio} até ${fim}`);
+
+for(let multiplicador = inicio; multiplicador <= fim; multiplicador++) {  /*Vai do número inicial até o final 
+                                                                    Representa os números cujas tabuadas serão exibidas*/
+    console.log(`\nTabuada de: ${multiplicador}`)
+
+    for(let contador = 0; contador <= 10; contador++) {  //Exibe a tabuada de 1 a 10 para o número atual do loop externo
+        let resultado = multiplicador * contador;
+        console.log(`${contador} x ${multiplicador} = ${resultado}`)
+    }
+}
+
