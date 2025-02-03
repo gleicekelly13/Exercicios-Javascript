7b11ace59a719eb4b22c1e1a3bb7dd10a8fbd906
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2025-02-03 10:02:08 -0300

    Update index.js

--- loops #6/Contador_digitos/index.js
+++ loops #6/Contador_digitos/index.js
@@ -4,8 +4,10 @@ Peça ao usuário um número inteiro e conte quantos dígitos ele tem usando um 
 
 import entradaDados from 'readline-sync';
 
+// Solicita um número inteiro ao usuário e converte para número
 let num = parseInt(entradaDados.question('Digite um numero inteiro: '));
 
+// Inicializa a variável para contar os dígitos
 let contador = 0;
 
 /*while(num >= 1) {
@@ -17,14 +19,20 @@ console.log(contador);*/
 
 //FORMA ALTERNATIVA
 /*while(num >= 1) {
-    num = Math.floor(num / 10);
-    contador++;
+    num = Math.floor(num / 10);  // Divide por 10 e remove a parte decimal
+    contador++;  // Conta um dígito
 }
 console.log(contador); */
 
-//FOR
+//FOR (Alternativa mais compacta)
 for(let i = num; i >= 1; i = Math.floor(i / 10)) {
-    contador++;
+    contador++;  // Conta um dígito a cada divisão
 } 
-console.log(contador)
+console.log(`O número tem ${contador} dígitos(s)`)
 
+
+/* 
+Enquanto o número for maior ou igual a 1, continuamos dividindo ele por 10.
+Cada vez que dividimos, significa que removemos um dígito.
+Incrementamos o contador a cada iteração.
+*/
