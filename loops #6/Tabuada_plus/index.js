840d3b3bcf759754ccbf949c404f87802fae794c
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2025-02-03 10:51:12 -0300

    Update index.js

--- loops #6/Tabuada_plus/index.js
+++ loops #6/Tabuada_plus/index.js
@@ -11,15 +11,23 @@ import entradaDados from 'readline-sync';
 let inicio = parseInt(entradaDados.question('Digite um numero inicial: '));
 let fim = parseInt(entradaDados.question('Digite um numero final: '));
 
-console.log(`\nTabuada de ${inicio} até ${fim}`);
+if(isNaN(inicio) || isNaN(fim)) {  //Verifica se o número é válido
+    console.log("Por favor, digite números válidos.");
 
-for(let multiplicador = inicio; multiplicador <= fim; multiplicador++) {  /*Vai do número inicial até o final 
+} else if(inicio > fim) {  //Verificação para evitar que inicio seja maior que fim
+    console.log("O número inicial deve ser menor ou igual ao número final.")
+
+} else {
+    console.log(`\nTabuada de ${inicio} até ${fim}`);
+
+    for(let multiplicador = inicio; multiplicador <= fim; multiplicador++) {  /*Vai do número inicial até o final 
                                                                     Representa os números cujas tabuadas serão exibidas*/
-    console.log(`\nTabuada de: ${multiplicador}`)
+        console.log(`\nTabuada de: ${multiplicador}`)
 
-    for(let contador = 0; contador <= 10; contador++) {  //Exibe a tabuada de 1 a 10 para o número atual do loop externo
-        let resultado = multiplicador * contador;
-        console.log(`${contador} x ${multiplicador} = ${resultado}`)
+        for(let contador = 0; contador <= 10; contador++) {  //Exibe a tabuada de 1 a 10 para o número atual do loop externo
+            let resultado = multiplicador * contador;
+            console.log(`${contador} x ${multiplicador} = ${resultado}`)
+        }
     }
-}
 
+}
