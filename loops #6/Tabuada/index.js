c7f8b13b92a878a899b0f0eea2d0c3c2b7256b39
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2025-02-03 10:34:18 -0300

    Update index.js

--- loops #6/Tabuada/index.js
+++ loops #6/Tabuada/index.js
@@ -6,13 +6,19 @@ Solicite ao usuário um número (pode usar prompt) e exiba a tabuada desse núme
 
 import entradaDados from 'readline-sync';
 
-let multiplicador =  entradaDados.question("Digite um numero para ver a tabuada: ");
-multiplicador = Number(multiplicador);
+let multiplicador =  Number(entradaDados.question("Digite um numero para ver a tabuada: "));
 
-for(let contador = 0; contador <= 10; contador++) {
+if(isNaN(multiplicador)) {  // Verificação para números inválidos.
+    console.log("Por favor, digite um número válido.")
+
+} else {
+    for(let contador = 0; contador <= 10; contador++) {
     let resultado = contador * multiplicador;
     console.log(`${contador} x ${multiplicador} = ${resultado}`);
+    }
 }
+
+
 
 
 
