1b9ef3b36708302c9f7b197ebae2baf597fc3ecc
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2025-02-07 09:55:43 -0300

    Create maiusculas.js

---
+++ Arrow_functions/maiusculas.js
@@ -0,0 +1,15 @@
+/*
+Crie uma arrow function que receba uma string e retorne a mesma string em letras maiúsculas.
+*/
+
+
+const converteParaMaiusculas = str => {
+    return str.toUpperCase();
+}
+
+console.log(converteParaMaiusculas("gato"));
+
+
+/*  FORMA SIMPLIFICADA EM UMA ÚNICA LINHA
+const converteParaMaiusculas = str => str.toUpperCase();
+*/
