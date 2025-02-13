6b4e679189127155b6c88a0fda92fb4602752a29
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2025-02-13 10:07:34 -0300

    Create listaNomes.js

---
+++ forEach/listaNomes.js
@@ -0,0 +1,9 @@
+/*
+Crie um array de nomes e use o forEach para imprimir cada nome no console.
+*/
+
+const nomes = ["Gleice", "Ana", "João", "Maria", "Pedro", "Lucas"];
+
+nomes.forEach(nome => {
+    console.log(nome);
+})
