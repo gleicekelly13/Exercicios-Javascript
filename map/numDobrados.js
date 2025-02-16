b4eb55db2360946bbceb7aba5639358a3a455597
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2025-02-16 11:26:59 -0300

    Create numDobrados.js

---
+++ map/numDobrados.js
@@ -0,0 +1,10 @@
+/*
+Dado um array de números, use o método map para criar um novo array com cada número dobrado
+*/
+
+let numeros = [2, 4, 6, 8, 10];
+
+let dobraNum = numeros.map(numero => numero * 2);
+console.log(dobraNum);
+
+
