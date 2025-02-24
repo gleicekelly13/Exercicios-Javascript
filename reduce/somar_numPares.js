168e0cb4ceada362e50fa3fa8562f869dda8821a
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2025-02-24 16:03:20 -0300

    Create somar_numPares.js

---
+++ reduce/somar_numPares.js
@@ -0,0 +1,34 @@
+/*
+Dado um array de números, use o reduce para somar apenas os números pares
+*/
+
+const numeros = [1, 2, 3, 4, 5, 6];
+
+let somaPares = numeros.filter(num => num % 2 === 0).reduce((acumulador, par) => {
+
+    return acumulador + par;
+}, 0);
+
+console.log(somaPares);
+
+
+
+/* FORMA ALTERNATIVA SEM UTILIZAR O FILTER
+let somaPares = numeros.reduce((acumulador, num) => 
+    num % 2 === 0 ? acumulador + num : acumulador, 0
+)
+
+console.log(somaPares);
+
+*/
+ 
+/*FUNÇÃO SEM O FILTER
+function somar_numPares (numeros) {
+    return numeros.reduce((acumulador, num) => 
+        num % 2 === 0 ? acumulador + num : acumulador, 0)
+};
+
+let numPares_somados = somar_numPares(numeros);
+
+console.log(numPares_somados);
+*/
