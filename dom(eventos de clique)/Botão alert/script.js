1cb49e40aab3cc72c55f4518908419960a8dc13d
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2025-03-07 10:15:32 -0300

    Código alternativo que atualiza um único elemento

--- dom(eventos de clique)/Botão alert/script.js
+++ dom(eventos de clique)/Botão alert/script.js
@@ -1,4 +1,5 @@
-document.body.addEventListener('keydown', (evento) => {  //O próprio evento pode ser passado como parâmetro
+//ADICIONA UM NOVO ELEMENTO <h2) A CADA TECLA PRESSIONADA
+/*document.body.addEventListener('keydown', (evento) => {  //O próprio evento pode ser passado como parâmetro
     const teclaPressionada = document.createElement('h2');
     teclaPressionada.innerText = `A tecla ${evento.key} foi pressionada!`;
     document.body.appendChild(teclaPressionada); //Exibe a tecla pressionada no body
@@ -6,3 +7,18 @@ document.body.addEventListener('keydown', (evento) => {  //O próprio evento pod
 })
 
 /* `evento.key` extrai o valor da tecla */
+
+
+//ATUALIZA UM ÚNICO ELEMENTO, SEM CRIAR VÁRIOS <h2>
+document.body.addEventListener('keydown', (evento) => {
+    let teclaPressionada = document.getElementById('tecla');
+
+    if(!teclaPressionada) {  // Se não existir, cria o <h2>
+        teclaPressionada = document.createElement('h2');
+        teclaPressionada.id = 'tecla';  // Atribui um id ao elemento criado
+        document.body.appendChild(teclaPressionada);  // Adiciona o <h2> ao body, tornando-o visível na página.
+    }
+
+    teclaPressionada.innerText = `A tecla ${evento.key} foi pressionada!`;
+    console.log(teclaPressionada);
+})
