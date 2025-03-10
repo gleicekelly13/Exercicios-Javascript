291897eeeb89909a055be216e1d76754b65a3594
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2025-03-10 09:11:43 -0300

    Criação das funções que atualizam o contador e que faz o  reset

--- dom(eventos de clique)/Atualizando contador/scripts.js
+++ dom(eventos de clique)/Atualizando contador/scripts.js
@@ -3,7 +3,6 @@ Atualizando um contador
 Objetivo: Criar um contador que aumenta e diminui ao clicar nos botões.
 
 Passos:
-
 Crie um <span> que exibe um número inicial (0).
 Adicione dois botões: "Aumentar" e "Diminuir".
 Ao clicar nos botões, o número deve ser atualizado corretamente.
@@ -14,21 +13,32 @@ Ao clicar nos botões, o número deve ser atualizado corretamente.
 const contador = document.getElementById('span');
 const aumentar = document.getElementById('aumenta');
 const diminuir = document.getElementById('diminui');
-
+const resetar = document.getElementById('reseta');
 
 let numero = 0
 
+function atualizarContador() {  // Evita código repetitivo ao atualizar o número
+    contador.textContent = numero;  // Atualiza o conteúdo do span(contador)
+}
+
 function aumentarContador () {
     numero++;  // Primeiro altera o valor
-    contador.textContent = numero;  // Depois atualiza o conteúdo do span(contador)
+    atualizarContador();
 }
 
 function diminuirContador () {
     if(numero > 0) {  //Verificação para o contador não ir abaixo de 0, ou seja, não números negativos
         numero--;
-        contador.textContent = numero;
+        atualizarContador()
     }
 }
 
+function resetarContador() {  // Função que zera o contador
+    numero = 0;  
+    atualizarContador();
+}
+
+
 aumentar.addEventListener('click', aumentarContador);
 diminuir.addEventListener('click', diminuirContador);
+resetar.addEventListener('click', resetarContador);
