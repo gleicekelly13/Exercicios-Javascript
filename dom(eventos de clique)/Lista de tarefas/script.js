427e68a3a5376ada4ac1203ed531c8ada0e76805
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2025-03-16 16:01:03 -0300

    Create script.js

---
+++ dom(eventos de clique)/Lista de tarefas/script.js
@@ -0,0 +1,35 @@
+/*
+Lista de tarefas (To-Do List)
+
+Crie um campo de texto e um botão "Adicionar".
+Ao clicar no botão, adicione o texto digitado a uma lista (<ul>).
+Cada item deve ter um botão "Remover" que exclui o item ao ser clicado.
+*/
+
+const tarefa = document.getElementById('texto');
+const addTarefa = document.getElementById('add');
+const lista = document.getElementById('lista');
+
+function criarTarefa () {
+    if(tarefa.value.trim() !== '') {
+        const newLi = document.createElement('li');
+        newLi.textContent = tarefa.value; 
+
+        const buttonRemover = document.createElement('button');
+        buttonRemover.textContent = 'Remover';
+
+        buttonRemover.addEventListener('click', () => {
+            newLi.remove();
+        })
+
+        newLi.appendChild(buttonRemover);
+        lista.appendChild(newLi);
+    
+        tarefa.value = "";
+    }
+    
+}
+
+addTarefa.addEventListener('click', criarTarefa);
+
+// `textContent` deifine o texto dentro de um elemento de forma mais prática, simples e eficiente
