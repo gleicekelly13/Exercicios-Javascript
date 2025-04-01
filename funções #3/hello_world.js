7c920a50f4e71c956399efa5250dba5778da17e8
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2025-04-01 16:48:18 -0300

    Create hello_world.js

---
+++ funções #3/hello_world.js
@@ -0,0 +1,16 @@
+// Write a function "greet" that returns "hello world!"
+function greet(text) {  // A função greet aceita um parâmetro chamado text, mas não faz nada com ele.
+    return "hello world!";  // Sempre retorna a string fixa "hello world!".
+  }  //A solução que o codewars  aceitou
+
+// Comum em exercícios para testar compreensão de parâmetros e retorno
+
+//Solução que eu fiz na primeira tentativa
+/*function greet (text) {  
+    return text;
+}
+
+console.log(greet("Hello World!")); */
+
+// Solução com arrow function
+// const greet = () => "hello world!";
